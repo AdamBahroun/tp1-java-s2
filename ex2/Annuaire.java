eb352994adf673dbe136e1e9f54a14e86450555f
Adambahroun <adambahroun54@gmail.com>
2026-02-19 13:24:28 +0100

    ajout de trie

--- ex2/Annuaire.java
+++ ex2/Annuaire.java
@@ -2,18 +2,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Annuaire { 
-    Map<String,Fiche> table;
+public class Annuaire {
+    Map<String, Fiche> table;
 
     public Annuaire() {
-        table = new TreeMap<>();
+        table = new HashMap<>();
     }
+
     public void ajouter(Fiche f) {
         table.put(f.getNom(), f);
     }
-     public Fiche rechercher(String nom) {
+
+    public Fiche rechercher(String nom) {
         return table.get(nom);
     }
+
     public void afficherTout() {
         if (table.isEmpty()) {
             System.out.println("L'annuaire est vide.");
@@ -22,5 +25,9 @@ public class Annuaire {
                 System.out.println(f);
             }
         }
+    }
+
+    public void trier() {
+        table = new TreeMap<>(table);
     }
 }
